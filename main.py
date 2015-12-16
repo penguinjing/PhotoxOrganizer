489dc27bf8c0375432bf3e250606373f6a8bedf1
Edward <penguinjing@gmail.com>
2015-12-17 04:07:36 +0800

    add copy_image_file function

--- main.py
+++ main.py
@@ -4,31 +4,40 @@
 文件说明：根据图片拍摄日期建立(-年/-月/-日)三级目录(备份资料库)，将文件相应
 拷贝至相应日期目录中，并将图片文件名改为拍摄日期(20151210)+原图片名
 作者信息：penguinjing & lixiguangzhou
-版本自述: 0.2.0
+版本自述: 0.3.0
 ...
 '''
 # 全局引用
-import exifread
 import os
+import exifread
+import shutil
 
 # 全局变量
 #PATH = "/path/2/work dir"
 
 # 函式撰写区
-def get_exif_date(filename='test_o.jpg'):
+def get_exif_date(FileName='./test_o.jpg'):
     tags = {}
-    with open(filename, 'rb') as f:
+    with open(FileName, 'rb') as f:
         tags =exifread.process_file(f, details=False)  #  Return Exif tags as a dictionary
-    ShotDateTime = tags.get('EXIF DateTimeOriginal').values
-    ShotDate = ShotDateTime[:10].split(':')
-    return ShotDate
+    shotDateTime = tags.get('EXIF DateTimeOriginal').values
+    shotDate = shotDateTime[:10].split(':')
+    return shotDate
 
 def create_target_dir(TargetDir, ShotDate):
     year, month, day = ShotDate
-    TargetDirJoin = os.path.join(TargetDir, year, month, day)
-    os.makedirs(TargetDirJoin)
+    targetDirJoin = os.path.join(TargetDir, year, month, day)
+    os.makedirs(targetDirJoin)
+
+def copy_image_file(FileName, TargetDir, ShotDate):
+    year, month, day = ShotDate
+    newFileName = year + month + day + '_' + os.path.basename(FileName)
+    targetFileName = os.path.join(TargetDir, year, month, day, newFileName)
+    shutil.copy(FileName, targetFileName)
+
 
 # 自检区
 if __name__ == '__main__':
-    print get_exif_date()
-    create_target_dir('.', get_exif_date())
+    image_date = get_exif_date()
+    create_target_dir(TargetDir = '.', ShotDate = image_date)
+    copy_image_file(FileName='./test_o.jpg', TargetDir = '.', ShotDate = image_date)
