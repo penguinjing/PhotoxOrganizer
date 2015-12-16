7355c4cb5bb62e0defaf72ed045c0297d79211ca
Edward <penguinjing@gmail.com>
2015-12-16 17:34:47 +0800

    add reading date from exif

--- README.md
+++ README.md
@@ -1,5 +1,21 @@
 # PhotoxOrganizer
 
+## 照片exif中读取拍摄日期
+采用模块读取字段 EXIF.Photo.DateTimeOriginal
+
+tags.get('EXIF DateTimeOriginal').values
+
+```
+import exifread
+
+filename = open(path_name, 'rb')
+tags =exifread.process_file(filename)  #  Return Exif tags as a dictionary
+```
+
+类似字段 EXIF.Photo.DateTimeDigitized
+
+Reference: [Standard Exif Tags from Metadata reference tables](http://www.exiv2.org/tags.html) at www.exiv2.org
+
 
 ## 有用的函数
 import os
@@ -16,3 +32,5 @@ os.path.sep() => / (POSIX) or \\ (Windows)
 
 os.path.exists(path)
 os.mkdir()
+
+from PIL.ExifTags import TAGS
--- main.py
+++ main.py
@@ -3,25 +3,25 @@
 '''
 文件说明：根据图片拍摄日期建立(-年/-月/-日)三级目录(备份资料库)，将文件相应
 拷贝至相应日期目录中，并将图片文件名改为拍摄日期(20151210)+原图片名
-作者信息：penguinjing
-版本自述: 0.0.1
+作者信息：penguinjing & lixiguangzhou
+版本自述: 0.1.0
 ...
 '''
 # 全局引用
-import sys
+import exifread
+
 # 全局变量
-PATH = "/path/2/work dir"
+#PATH = "/path/2/work dir"
+
 # 函式撰写区
-def foo():
-    pass
-    return None
+def get_exif_date(filename='test_o.jpg'):
+    tags = {}
+    f = open(filename, 'rb')
+    tags =exifread.process_file(f)  #  Return Exif tags as a dictionary
+    ShotDateTime = tags.get('EXIF DateTimeOriginal').values
+    ShotDate = ShotDateTime[:10].split(':')
+    return ShotDate
 
 # 自检区
 if __name__ == '__main__':
-    if 1 != len(sys.argv):
-        print '''Usage:
-$ python main..py
-        '''
-    else:
-        foo()
-    
+    print get_exif_date()
